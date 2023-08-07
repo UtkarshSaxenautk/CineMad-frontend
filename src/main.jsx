cd76e662010c11f9bff549844160e408447b01de
Utkarsh Saxena <82103333+UtkarshSaxenautk@users.noreply.github.com>
2023-08-07 11:59:17 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+//import App component 
 import App from './App'
 import './index.css'
-
+// create Root then render App component Strict mode makes it easier to write "secure" JavaScript
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
